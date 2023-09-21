63d662a8a808a87b9aceefd2ccc158e5338d37ee
bouhairi tarik <bouhairi.tarik@gmail.com>
2023-09-21 13:26:11 -0400

    push1

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,28 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int somme = 0;
+            int numero;
+
+            Console.WriteLine("Entrez des numéros (entrez 0 pour arrêter) :");
+
+            do
+            {
+                Console.Write("Entrez un numéro : ");
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    somme += numero;
+                }
+                else
+                {
+                    Console.WriteLine("Ce n'est pas un numéro valide. Réessayez.");
+                }
+            } while (numero != 0);
+
+            Console.WriteLine($"La somme des numéros entrés est : {somme}");
+        }
     }
 }
